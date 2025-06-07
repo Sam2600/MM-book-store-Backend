95df70217100d0d5318f4f43dc480e546e0b8b40
Kaung Htet San <kghtetsan26@gmail.com>
2025-06-07 11:59:12 +0630

    feat(localization): add english language files for pagination, auth, passwords, messages and validation
    Add initial set of English language files to support application localization.
    Includes files for pagination, authentication, password reset, custom messages,
    and validation rules. These files provide the foundation for internationalization.

---
+++ lang/en/message.php
@@ -0,0 +1,53 @@
+<?php
+
+return [
+
+
+    //for success
+    "SS001" => ":attribute registered successfully.",
+    "SS002" => ":attribute updated successfully.",
+    "SS003" => ":attribute deleted successfully.",
+    "SS004" => "Login Successful.",
+    "SS005" => "Logout Successful.",
+    "SS006" => ":attribute sent successfully.",
+    "SS007" => "This user is authenticate.",
+
+    //for error
+    "SE001" => "Fail to Save Data!",
+    "SE002" => "Fail to Delete Data!",
+    "SE003" => "Fail to Update Data!",
+    "SE004" => ":attribute is not found!",
+    "SE005" => "This :attribute is already deleted!",
+    "SE006" => "This :attribute is already exist!",
+    "SE007" => "Fail to Download!",
+    "SE008" => "Wrong credentials. Please try again!",
+    "SE009" => "Failed to fetch :attribute. Please try again!",
+    "SE010" => "Internal Server Error!",
+    "SE011" => ":attribute1 must not be greater than :attribute2 MB!",
+    "SE012" => "Failed to sent :attribute. Please try again!",
+    "SE013" => "This login user needs to be activated!",
+    "SE014" => "This user is unauthenticated!",
+    "SE015" => "Failed to store files!",
+    "SE016" => ":attribute does not exist!",
+    "SE017" => "This :attribute1 is already used in some :attribute2!",
+
+    //for attribute
+    "Data" => "Data",
+    "Counterparty" =>  "Counterparty",
+    "Contract" => "Contract",
+    "Contract Type" => "Contract Type",
+    "Life Cycle" => "Life Cycle",
+    "User" => "User",
+    "Template Code" => "Template Code",
+    "Mail" => "Mail",
+    "Password" => "Password",
+    "Menu" => "Menu",
+    "Attach File" => "Attach File",
+    "Chosen file" => "Chosen file",
+    "2" => "2",
+    "contracts" => "contracts",
+    "templates" => "templates",
+    "Life Cycle Type" => "Life Cycle Type",
+    "This Email Address" => "This Email",
+    "This File" => "This file",
+];
